56b8e6b5c8e91acf72ab09be30366ecbb361d2c8
nopi96912 <nopiwork@gmail.com>
2017-07-26 21:53:11 +0200

    Shorten score method

--- bowling.py
+++ bowling.py
@@ -2,43 +2,47 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
+    for turn in range(len(game)):
+        if game[turn] == '/':
             result += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+            result += get_value(game[turn])
+        result += add_result(game, turn, frame)
+        last = get_value(game[turn])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[turn] == 'X' or game[turn] == 'x':
             in_first_half = True
             frame += 1
     return result
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    not_numbers = ['X', 'x', '/']
+    if char in numbers:
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char in not_numbers:
         return 10
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
+
+def add_result(string, index, frame):
+    result = 0
+    if frame < 10 and get_value(string[index]) == 10:
+        if string[index] == '/':
+            result += get_value(string[index+1])
+        elif string[index] == 'X' or string[index] == 'x':
+            result += get_value(string[index+1])
+            if string[index+2] == '/':
+                result += 10 - get_value(string[index+1])
+            else:
+                result += get_value(string[index+2])
+    return result
